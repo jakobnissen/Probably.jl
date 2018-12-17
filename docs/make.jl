f94276efec01d505096d64a0d139850552f86fe2
Ben J. Ward <ward9250@gmail.com>
2018-12-17 15:35:54 +0000

    Rename home.md to index.md

--- docs/make.jl
+++ docs/make.jl
@@ -2,7 +2,7 @@ using Documenter, Probably
 
 makedocs(sitename="Probably.jl", html_prettyurls=false,
         pages = Any[
-        "Home" => "home.md",
+        "Home" => "index.md",
         "HyperLogLog" => "hyperloglog.md",
         "Count-min sketch" => "countmin.md",
         "Cuckoo filter" => "cuckoo.md",
